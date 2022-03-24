aa2e1bcfee1a00162bb238556c4708ccd2e02976
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-03-24 17:08:52 -0300

    Update index.js and App.js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import Calculator from './components/Calculator';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+// import Calculator from './components/Calculator';
+
+import './style/App.css';
+import HomePage from './pages/HomePage';
+import CalculatorPage from './pages/CalculatorPage';
+import QuotePage from './pages/QuotePage';
+import FrontPage from './components/FrontPage';
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
+  <>
+    <header>
+      <Frontpage />
+    </header>
+    <main>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<QuotePage />} />
+      </Routes>
+    </main>
+  </>
 );
 
 export default App;
