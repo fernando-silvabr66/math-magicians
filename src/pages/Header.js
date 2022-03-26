360267de41705deb65be43f9227ed6f42bee066c
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-03-26 13:51:41 -0300

    Layout and functions fix

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,14 +3,11 @@ import '../style/Header.css';
 
 const Header = () => (
   <header className="header">
-    <Link to="/">
-      <h1>Math Magicians</h1>
-    </Link>
     <nav className="nav">
       <h1 className="nav-title">Math Magicians</h1>
       <ul className="nav-links">
         <li>
-          <Link to="/" className="nav-item">Home</Link>
+          <Link to="/home" className="nav-item">Home</Link>
         </li>
         <li>
           <Link to="/calculator" className="nav-item">Calculator</Link>
--- src/pages/QuotePage.jsx
+++ src/pages/QuotePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/QuotePage.css';
 
 const QuotePage = () => (
   <>
